7f0148a51d47bf707d47ce94a280ecea357c42af
Christian Morales <atticuschris@yahoo.com>
2019-04-07 19:22:31 -0500

    DFDF

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,21 +73,21 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                <img src="https://bongoutindonesia.com/assets/img/bg_if_1.svg" />
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
